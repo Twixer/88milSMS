13f86037cedb2c9bc416cd8ca5d5588411c08e7b
twixer <twixer@gmail.com>
2014-09-22 00:27:26 +0200

    feat(script) add hist analysis (day, hour)

--- exploratory_analysis.R
+++ exploratory_analysis.R
@@ -7,3 +7,18 @@
 # Load the data
 source("getting_cleaning_saving_data.R")
 data <- load.data()
+
+# histograms
+hist(data$timestamp, 
+     breaks="days", 
+     main="Histogram of SMS by day", 
+     xlab="Days",
+     ylab="Frequency")
+
+hist(data$timestamp, 
+     breaks="hours",
+     main="Histogram of SMS by hour", 
+     xlab="Hours",
+     ylab="Frequency")
+
+boxplot(data$timestamp ~ data$id.mobile)
