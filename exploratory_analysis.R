4426f5b12b750ef4fd842d2f6f37b20b45bb3930
twixer <twixer@gmail.com>
2014-09-22 00:17:00 +0200

    feat(script) add a new script to begin the exploratory data analysis

---
+++ exploratory_analysis.R
@@ -0,0 +1,9 @@
+# =============================================================================
+#
+# We will try some exploratory data analysis on the data.
+#
+# =============================================================================
+
+# Load the data
+source("getting_cleaning_saving_data.R")
+data <- load.data()
